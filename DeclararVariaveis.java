8fc2bc12c9eb5fafa11c82fe359fe4d9a6089eea
Raphael M. <raphael.hkiller@gmail.com>
2025-05-29 19:36:14 -0300

    Delete DeclararVariaveis.java

--- DeclararVariaveis.java
+++
@@ -1,7 +0,0 @@
-public class DeclararVariaveis{
-    public static void main(String[]args){
-        int minhaIdade = 20;
-        String meuNome = "Raphael";
-        var nomeVariavel = "Raphael";
-    }
-}  
