5beb4adcba36128b460a3298e0721bc328d46478
Raphael Henrique <raphael.hkiller@gmail.com>
2025-06-02 07:24:39 -0300

    Add files via upload

---
+++ ExFour.java
@@ -0,0 +1,15 @@
+/*Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+decimais*/
+import java.util.Scanner;
+public class ExFour{
+    public static void main(String[]args){
+    Scanner scanner = new Scanner(System.in);    
+    System.out.println("Digite o número do funcionário: ");
+    int funcionario = scanner.nextInt(); 
+    System.out.println("Quantas horas trabalhadas? ");
+    double horasTrabalhadas = scanner.nextDouble();
+    double salario = horasTrabalhadas * 10;
+    System.out.printf("O salário do funcionário de número %d corresponde a %.2f ",funcionario, salario);
+    }
+}
