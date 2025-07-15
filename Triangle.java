7c5ccb4cb2e8da1a4531aaf0301f58ddab5561ad
Raphael Henrique <raphael.hkiller@gmail.com>
2025-07-15 19:47:53 -0300

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Triangle {
+    public double A;
+    public double B;
+    public double C;
+
+    public double calcularArea() {
+        double p = (A + B + C) / 2.0;
+        return Math.sqrt(p * (p - A) * (p - B) * (p - C));
+    }
+}
