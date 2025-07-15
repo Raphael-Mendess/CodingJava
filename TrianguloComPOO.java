58d5c2c145a7bcaf6ae1039405802f7278e35093
Raphael Henrique <raphael.hkiller@gmail.com>
2025-07-15 19:46:40 -0300

    Add files via upload

---
+++ TrianguloComPOO.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import modelo.Triangle;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Triangle x = new Triangle();
+        Triangle y = new Triangle();
+
+        System.out.println("Digite os lados do triângulo X:");
+        x.A = sc.nextDouble();
+        x.B = sc.nextDouble();
+        x.C = sc.nextDouble();
+
+        System.out.println("Digite os lados do triângulo Y:");
+        y.A = sc.nextDouble();
+        y.B = sc.nextDouble();
+        y.C = sc.nextDouble();
+
+        double areaX = x.calcularArea();
+        double areaY = y.calcularArea();
+
+        System.out.printf("Área do triângulo X: %.4f\n", areaX);
+        System.out.printf("Área do triângulo Y: %.4f\n", areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Maior área: Triângulo X");
+        } else if (areaY > areaX) {
+            System.out.println("Maior área: Triângulo Y");
+        } else {
+            System.out.println("As áreas são iguais");
+        }
+
+        sc.close();
+    }
+}
