aca90f075f7b3717db2e876db850f4b5b3df00f2
Raphael Henrique <raphael.hkiller@gmail.com>
2025-07-18 21:07:56 -0300

    Update DinheiroNaCarteira.java

--- DinheiroNaCarteira.java
+++ DinheiroNaCarteira.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class DinheiroNaCarteira{
     public static void main(String[]args){
     Scanner ler = new Scanner(System.in);
+        
     System.out.println("Digite 1 para contar de 0 até 50.000 mil");
     System.out.println("Digite 2 para contar de 0 até 100.000 mil");
+        
     int numero = ler.nextInt();
+        
             if (numero == 1){
             for (int i = 0; i <=50000; i++){
                System.out.println(i);
@@ -20,5 +23,7 @@ public class DinheiroNaCarteira{
         }else if(numero ==2){
           System.out.println("Parabéns, agora você tem 100.000 mil na conta");  
         }
+
+        
     }
 }
