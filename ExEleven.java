06ea9d1b2b4c0b6f88f64696b8085aaf4cff7303
Raphael Henrique <raphael.hkiller@gmail.com>
2025-07-18 21:13:59 -0300

    Update ExEleven.java

--- ExEleven.java
+++ ExEleven.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class ExEleven {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){  
+        
         Scanner lerVariavel = new Scanner(System.in);
+        
         int soma = 0;
+        
         int numero = lerVariavel.nextInt();
+        
         for (int i = 0; i < numero; i++) {               //Inicialização, condição, incremento(atualização)
             int x = lerVariavel.nextInt();
             soma = soma + x;
         }
+        
         System.out.printf("O valor total dos números que você digitou corresponde a %d%n", soma);
 
         lerVariavel.close();
-    }
+      }
 }
