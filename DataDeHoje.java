61c1d761e01d9f236fcb32074771a6f8584cee53
Raphael Henrique <raphael.hkiller@gmail.com>
2025-07-23 20:02:38 -0300

    Update DataDeHoje.java

--- DataDeHoje.java
+++ DataDeHoje.java
@@ -1,13 +1,18 @@
 import java.util.Date;
+
 public class DataDeHoje{
     public static void main(String[] args) {
+        
         Date relogio = new Date();
+        
         System.out.print("A hora do sistema é:");
         System.out.println(relogio.toString());
 
         boolean bool = true;
         if(bool){
-        System.out.print("Verdadeiro");
+           System.out.println("Verdadeiro");
+        }else{
+           System.out.println("Falso"); 
         }
     }
 }
