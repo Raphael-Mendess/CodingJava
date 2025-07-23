a1189c659932c5ecdbd4421365dc7366188110c3
Raphael Henrique <raphael.hkiller@gmail.com>
2025-07-22 22:47:33 -0300

    Update ExFour.java

--- ExFour.java
+++ ExFour.java
@@ -2,13 +2,18 @@
 hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
 decimais*/
 import java.util.Scanner;
+
 public class ExFour{
     public static void main(String[]args){
-    Scanner scanner = new Scanner(System.in);    
+        
+    Scanner scanner = new Scanner(System.in); 
+        
     System.out.println("Digite o número do funcionário: ");
     int funcionario = scanner.nextInt(); 
+        
     System.out.println("Quantas horas trabalhadas? ");
     double horasTrabalhadas = scanner.nextDouble();
+        
     double salario = horasTrabalhadas * 10;
     System.out.printf("O salário do funcionário de número %d corresponde a %.2f ",funcionario, salario);
 
