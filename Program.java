d8207a48acfbb9c94b22574e95583ac950e892aa
Raphael Henrique <raphael.hkiller@gmail.com>
2025-07-28 15:33:17 -0300

    Update Program.java

--- Program.java
+++ Program.java
@@ -8,23 +8,19 @@ public class Program {
         
        Locale.setDefault(Locale.US); 
        Scanner lerVar = new Scanner(System.in);
-
        
        Product product = new Product();
        System.out.println("Enter product data: ");
 
-
        System.out.print("Name:");
        product.name = lerVar.nextLine(); 
-
 
        System.out.print("Price:");
        product.price = lerVar.nextDouble();
 
-
        System.out.print("Quantity in Stock:");
        product.quantity = lerVar.nextInt();
-       
+     
        System.out.println(); 
        System.out.println("Product Data:" + product);
 
@@ -35,16 +31,17 @@ public class Program {
 
        System.out.println();
        System.out.println("Update Data" + product);
-       
+      
        System.out.println();
        System.out.print("Enter the numbers of products to be removed from stock");
        quantity = lerVar.nextInt();
-       
+       product.removeProducts(quantity);
+
+       System.out.println();
+       System.out.println("Update Data" + product);
 
        System.out.println(product.name + ", " + product.price + ", " + product.quantity); 
 
        lerVar.close();
     }
 }
-
-
