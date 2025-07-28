b04563b88574a13c721542f011bb5864389d0f35
Raphael Henrique <raphael.hkiller@gmail.com>
2025-07-28 10:39:48 -0300

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,4 +1,5 @@
 package application;
+import controller.Product;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,20 +8,30 @@ public class Program {
         
        Locale.setDefault(Locale.US); 
        Scanner lerVar = new Scanner(System.in);
+
        
        Product product = new Product();
        System.out.println("Enter product data: ");
 
+
        System.out.print("Name:");
        product.name = lerVar.nextLine(); 
+
 
        System.out.print("Price:");
        product.price = lerVar.nextDouble();
 
+
        System.out.print("Quantity in Stock:");
        product.quantity = lerVar.nextInt();
+       
+
+       System.out.println(product.toString());
+       
 
        System.out.println(product.name + ", " + product.price + ", " + product.quantity); 
+
        lerVar.close();
     }
 }
+
