94bdc8e28d43bb22f195dcdf5b5c7b7e6ae9bfa4
Raphael Henrique <raphael.hkiller@gmail.com>
2025-07-28 10:40:12 -0300

    Update Product.java

--- Product.java
+++ Product.java
@@ -8,19 +8,25 @@ public class Product {
 
     public double totalValueInStock(){
         return price * quantity;
-
     }
     
     public void addProducts(int quantity){
         this.quantity += quantity;
     }
 
-
     public void removeProducts(int quantity){
         this.quantity -= quantity;
     }
 
-    public String toString(){
-        return name;
-    }
+  
+public String toString() {
+    return name 
+        + ", $ " 
+        + String.format("%.2f", price)
+        + ", " 
+        + quantity 
+        + " units, Total: $ " 
+        + String.format("%.2f", totalValueInStock());
+        }
 }
+//Aqui estão os atributos e os métodos da minha classe Product;
