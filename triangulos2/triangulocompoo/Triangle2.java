0cbad3d8698123a1d055c1886ec7d83741aae434
Raphael Henrique <raphael.hkiller@gmail.com>
2025-08-04 15:07:42 -0300

    Create Triangle2.java

---
+++ triangulos2/triangulocompoo/Triangle2.java
@@ -0,0 +1,13 @@
+//Classe com atributos para representar um triângulo 
+package triangulocompoo;
+
+public class Triangle2 {
+    public double a;
+    public double b;
+    public double c;
+
+    public double calcularArea() {
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
