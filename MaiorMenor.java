13d39cc62d4a49463dcbb36a52c259f839588749
Raphael Henrique <raphael.hkiller@gmail.com>
2025-08-07 08:25:52 -0300

    Update MaiorMenor.java

--- MaiorMenor.java
+++ MaiorMenor.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class MaiorMenor{
     public static void main(String[]args){
+        
             Scanner scanner = new Scanner(System.in);
+        
             System.out.println("Digite o valor 1:");
             double n1 = scanner.nextDouble();
             
@@ -11,24 +13,27 @@ public class MaiorMenor{
             System.out.println("Digite o valor 3:");
             double n3 = scanner.nextDouble();
                
-            //Verificando qual é o menor
             double menor = n1;
+        
             if (n1 > n2 && n3 > n2){
                 menor = n2;
             }else if (n2 > n3 && n1 > n3){
                 menor = n3;
             }  
-            //verificando qual é o maior
+         
             double maior = n1;
+        
             if (n2 > n1 && n2 > n3){
                 maior = n2;
             }else if(n3 > n1 && n3 > n2){
                 maior = n3;
             }
-            //Escrevendo na tela os resultados
-            System.out.println("O maior valor digitado foi " + maior);
-            System.out.println("O menor valor digitado foi " + menor);
+        
+           System.out.println("O maior valor digitado foi " + maior);
+           System.out.println("O menor valor digitado foi " + menor);
+        
             scanner.close();
+        
             }
      }                         
      
@@ -36,4 +41,5 @@ public class MaiorMenor{
      
     
       
+
   
