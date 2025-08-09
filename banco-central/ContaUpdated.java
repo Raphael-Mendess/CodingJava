c33512d91b97838d522e597a6ee3489af6265eac
Raphael Henrique <raphael.hkiller@gmail.com>
2025-08-08 22:13:24 -0300

    Create ContaUpdated.java

---
+++ banco-central/ContaUpdated.java
@@ -0,0 +1,37 @@
+package contabancaria;
+
+public class ContaUpdated {  
+
+    public String name;
+    public int numberAccount;
+    public double deposit;
+
+      
+  public ContaUpdated(int numberAccount, String name, double Initialdeposit) {
+        this.numberAccount = numberAccount;
+        this.name = name;
+        this.deposit = Initialdeposit; // ou usar método deposit()
+    }
+   
+  public void addMoney(double amount) {
+        this.deposit = deposit + amount;        
+    }
+
+    public void withdrawMoney(double amount) {
+    if(amount <= this.deposit) {
+        this.deposit = deposit - amount;
+    } else {
+        System.out.println("Saldo insuficiente!");
+    }
+}
+
+    public String toString() {
+        return "Account: "
+        + numberAccount
+        + " , Holder: " 
+        + name
+        + ", "
+        + String.format("Balance $ %.2f", deposit);        
+    }
+}
+    
